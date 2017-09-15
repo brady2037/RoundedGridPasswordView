26156be8867b2e73326a471245a0a2702eac4540
brady lee <brady2037@outlook.com>
2017-09-15 11:36:33 +0800

    init project

---
+++ app/src/main/java/io/github/brady2037/rounded/gridpasswordview/MainActivity.java
@@ -0,0 +1,13 @@
+package io.github.brady2037.rounded.gridpasswordview;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
